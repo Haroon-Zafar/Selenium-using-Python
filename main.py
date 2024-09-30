15c4f2b5d564e5ce90f529ebe3bfa110d17f9913
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2024-09-30 18:11:58 +0500

    maximizing window and button clicking using XPATH

--- main.py
+++ main.py
@@ -4,14 +4,11 @@ from selenium.webdriver.common.by import By
 
 def selenium_textbox():
     chrome_driver = webdriver.Chrome()
-    chrome_driver.get("https://demoqa.com/text-box")
-    first_name = chrome_driver.find_element(By.ID, "userName")
-    first_name.send_keys("John")
+    chrome_driver.get("https://demoqa.com/buttons")
+    chrome_driver.maximize_window()
+    click_button = chrome_driver.find_element(By.XPATH, "/html/body/div[2]/div/div/div/div[2]/div[2]/div[3]/button")
+    click_button.click()
     time.sleep(5)
-    first_name.clear()
-    time.sleep(5)
-    # print("Chrome Initiated")
-
 
 if __name__ == "__main__":
     selenium_textbox()
