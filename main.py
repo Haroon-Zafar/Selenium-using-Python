c7693d75d663208ae641a305540d18b8330f3829
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2024-10-02 11:40:15 +0500

    is_displayed() method.

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ def selenium_textbox():
     chrome_driver.maximize_window()
     click_button = chrome_driver.find_element(By.XPATH, "/html/body/div[2]/div/div/div/div[2]/div[2]/div[3]/button")
     click_button.click()
+    dynamic_message = chrome_driver.find_element(By.ID, 'dynamicClickMessage')
+    if dynamic_message.is_displayed():
+        print("The Element is displayed")
+    else: 
+        print("The Element is NOT displayed")
+    
     time.sleep(5)
 
 if __name__ == "__main__":
