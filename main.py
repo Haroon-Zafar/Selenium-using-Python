110969c938fa30a0df179e746ae4f5bb15be7256
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2024-10-08 19:01:06 +0500

    Test keyword in class and test keyword in method()

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ from selenium.webdriver.support import expected_conditions as EC
 
 # Defining setup and tearDown methods in one class
 # It will help us to reduce redundancy of setup and tearDown class
-class MySetup(unittest.TestCase):
+# MAKE SURE TO MAKE `Test` infront of the class name 
+class TestMySetup(unittest.TestCase):
 
     # pytest allows us to execute specific set of actions
     @pytest.fixture
@@ -19,48 +20,9 @@ class MySetup(unittest.TestCase):
         yield chrome_driver
         chrome_driver.quit()    
 
-    # # defining a description method
-    # def short_description(self):
-    #     return ("This is the test case for Login Functionality")
-    
-    # def setup(self):
-    #     self.driver = webdriver.Chrome()
-    #     self.driver.get("https://demoqa.com/buttons")
-    #     self.driver.maximize_window()
-
-    # def tearDown(self):
-    #     return super().tearDown()
-        
-
-# Defining Test Cases in another 
-class MyTestCases(MySetup):
-    # without `self` keyword we cannot define methods in our classes
-    # @unittest.skip("Need to be updated after sprint 2")
-    # Above line will skip this particular test case and rest of the test cases will run
-    def test_unit_test(self):
-
-        short_description = self.short_description()
-        print(short_description)
-        
-        # click_button = self.driver.find_element(By.XPATH, "/html/body/div[2]/div/div/div/div[2]/div[2]/div[3]/button")
-        # click_button = self.driver.find_element(By.ID, "C1SRU")
-        # click_button.click()
-
-        try:
-            element = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(By.ID, 'dynamicClickMessage'))
-        
-        finally:
-            print("The element has been found")
-            expected_result = "You have done a dynamic"
-            actual_result = element.text
-            assert expected_result == actual_result
-        # time.sleep(10) pauses code execution exactly 10 seconds. It also stops the execution of the current thread
-        # time.sleep(3)
-
-        # driver_name.implicitly_wait(10) waits maximum 10 seconds for element's presence. If it is found after 2 seconds then code execution will be continued without wait for more 8 seconds.
-        # chrome_driver.implicitly_wait(3)
-        self.driver.implicitly_wait(3)
-    
+    # Test case name must start with `test` keyword
+    def test_navigate_to_google(self, browser):
+        pass
 
 
 if __name__ == "__main__":
